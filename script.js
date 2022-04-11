3f756c4844688e3c7e4c5cc52f33762450dc4364
lucasbatista <lucasbatista2408@gmail.com>
2022-04-11 18:16:01 -0300

    final-version2.0

--- script.js
+++ script.js
@@ -14,10 +14,14 @@ const listaMeme = [
 {img: "/images/unicornparrot.gif", nome: "meme7"},
 {img: "/images/unicornparrot.gif", nome: "meme7"}]
 
+const checkpair =[]
 const newList = []
+let tentativas;
+let qtdCartas;
+let acertos = 0;
 
 function gameStart(){
-  let qtdCartas = prompt("Quantas cartas? (4 à 14)")
+  qtdCartas = prompt("Quantas cartas? (4 à 14)")
   let cards = document.querySelector(".grid");
 
     // SE O NUMERO NÃO FOR VALIDO, A FUNÇÃO É INICIADA ATÉ QUE O NUMERO SEJA VALIDO
@@ -35,12 +39,12 @@ function gameStart(){
     // SE O NUMERO FOR VALIDO:
     for (i = 0; i < qtdCartas; i++){
         cards.innerHTML += `          
-        <div class="card">
+        <div class="card" data-framework ="${newList[i].nome}">
         <div class="front-parrot">
-          <img src= "${newList[i].img}" alt"${newList[i].nome}" onclick=""">
+          <img src= "${newList[i].img}" alt"${newList[i].nome}"">
           </div>
           <div class="back-parrot">
-          <img src= "/images/front.png" alt"${newList[i].nome}" onclick=""">
+          <img src= "/images/front.png" alt"${newList[i].nome}"">
           </div>
         </div>`
     }
@@ -48,17 +52,73 @@ function gameStart(){
   }
 gameStart()
 
-// CHECK PAIR //
+// VIRAR CARTA & CHECK PAIR
+const cards = document.querySelectorAll('.card');
+console.log(cards)
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+contador = 0;
 
-function checkPair(){
-  let card1 = document.querySelector(".")
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    contador += 1
+    console.log(contador)
+    return;
+  }
+
+  secondCard = this;
+  contador += 1;
+  console.log(contador)
+  checkForMatch();
 }
 
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
+  isMatch ? disableCards() : unflipCards();
+  
+}
 
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  resetBoard();
+  acertos ++;
+  setTimeout (gameEnding, 1000); 
+}
 
+function unflipCards() {
+  lockBoard = true;
 
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function gameEnding(){
+  if(acertos === qtdCartas/2){
+    alert(`GAME, ${contador}`)
+  }
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+// SORTEIO DAS CARTAS
 function sortear() { 
 	return Math.random() - 0.5; 
 }
